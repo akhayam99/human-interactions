863c82a240f75520bb61259a4adc44f283b9476c
Amin Khayam <aminkhayam@fattureincloud.it>
2023-01-19 17:08:40 +0100

    Install proper packages for navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View, ViewStyle } from 'react-native';
 import Account from './sections/account/Account';
 import Feedback from './sections/feedback/Feedback';
 import Monitor from './sections/monitor/Monitor';
@@ -10,47 +9,18 @@ import Search from './sections/search/Search';
 
 const Stack = createNativeStackNavigator();
 
-
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <View style={{ flex: 1, backgroundColor: '#E4F0F5' }}>
-        <View style={{ flex: 98 }}>
-          <View style={{ flex: 94 }}>
-            <View style={{ ...contentStyle }}>
-              <Stack.Navigator initialRouteName="Account">
-                <Stack.Screen name="Account" component={Account} />
-                <Stack.Screen name="Feedback" component={Feedback} />
-                <Stack.Screen name="Monitor" component={Monitor} />
-                <Stack.Screen name="Publish" component={Publish} />
-                <Stack.Screen name="Search" component={Search} />
-              </Stack.Navigator>
-            </View>
-          </View >
-          <View style={{ ...navigationStyle }}>
-            <Text> Bottom Navigation Bar Here </Text>
-          </View>
-        </View >
-        <View style={{ flex: 2 }} />
-      </View >
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Account" component={Account} />
+        <Stack.Screen name="Feedback" component={Feedback} />
+        <Stack.Screen name="Monitor" component={Monitor} />
+        <Stack.Screen name="Publish" component={Publish} />
+        <Stack.Screen name="Search" component={Search} />
+      </Stack.Navigator>
     </NavigationContainer >
   );
-}
-
-const contentStyle: ViewStyle = {
-  flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-}
-
-const navigationStyle: ViewStyle = {
-  alignItems: 'center',
-  borderBottomColor: '#428AA3',
-  borderBottomWidth: 1,
-  borderStyle: 'solid',
-  flex: 6,
-  justifyContent: 'center',
-  marginBottom: 6,
 }
 
 export default App;
--- src/app/sections/account/Account.tsx
+++ src/app/sections/account/Account.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 function Account(): JSX.Element {
   return (
-    <View>
+    <View style={{ flex: 100, backgroundColor: 'red' }}>
       <Text> Account Works </Text>
     </View >
   );
